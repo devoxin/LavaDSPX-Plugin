b8b31c4163f39e3202cf044c00a8b51a3f892523
Devoxin <lukel9914@gmail.com>
2023-12-23 14:55:27 +0000

    Update LavaDSPX to reduce jar size.

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "me.devoxin.lavadspx.plugin"
-version = "0.0.4"
+version = "0.0.5"
 
 lavalinkPlugin {
     name = "plugin-template"
@@ -33,7 +33,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.github.Devoxin:LavaDSPX:2.0.0")
+    implementation("com.github.Devoxin:LavaDSPX:2.0.1")
 }
 
 publishing {
